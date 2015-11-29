8f537601f8bb8c070ea0117bb13aad43ebc55c81
Subhash Ravisankar <subhash@appamark.com>
2015-11-29 02:59:21 -0800

    Add completed filter

--- server.js
+++ server.js
@@ -34,7 +34,18 @@ var todoNextId = 1;
 
 //GET todo list 
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && 
+		queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed : true});
+	} else if(queryParams.hasOwnProperty('completed') && 
+		queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed : false});
+	} 
+
+	res.json(filteredTodos);
 });
 
 //GET todo list 
