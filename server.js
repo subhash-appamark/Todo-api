69215951dad877c4171f5875643fda71025d7a50
Subhash Ravisankar <subhash@appamark.com>
2015-11-29 01:25:17 -0800

    Add post for todos

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 //var middleware = require("./middleware.js");
 //app.use(middleware.logger);
 
 //todos collection
-var todos = [{
+/*var todos = [{
 	id: 1,
 	description: "Take family for lunch",
 	completed: false
@@ -22,7 +25,11 @@ var todos = [{
 
 app.get('/', function (req, res) {
 	res.send("TODO API Root!");
-});
+});*/
+
+var todos = [];
+var todoNextId = 1;
+
 
 //GET todo list 
 app.get('/todos', function (req, res) {
@@ -46,6 +53,28 @@ app.get('/todos/:id', function (req, res) {
 		//to send 404
 		res.status(404).send();
 	}
+	
+});
+
+//POST Testing
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	/* 
+	var todo = {};
+	todo.id = todoNextId;
+	todo.description = body.description;
+	todo.completed = body.completed;
+	todos.push(todo);
+	todoNextId++;
+	res.json(todo);
+	*/
+	//OR
+
+	body.id = todoNextId++;
+	todos.push(body);
+	
+	res.json(todos);
 	
 });
 
